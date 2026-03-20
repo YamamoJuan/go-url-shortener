79cb802f127a4677a42c7e7da738420e9bb5696b
Yamamo Juan <yamamojuan@gmail.com>
2026-03-20 11:24:22 +0700

    fix base url for deploy

--- handler/handlers.go
+++ handler/handlers.go
@@ -1,29 +1,33 @@
-package handler 
+package handler
 
 import (
 	"net/http"
+	"os"
 	"strings"
+
 	"github.com/YamamoJuan/go-url-shortener/shortener"
 	"github.com/YamamoJuan/go-url-shortener/store"
 	"github.com/gin-gonic/gin"
 )
 
-type UrlCreationRequest struct{
+type UrlCreationRequest struct {
 	LongUrl string `json:"long_url" binding:"required"`
-	UserId string `json:"user_id" binding:"required"`
+	UserId  string `json:"user_id" binding:"required"`
 }
 
-func normalizeUrl(url string) string{
+func normalizeUrl(url string) string {
 	if !strings.HasPrefix(url, "http://") &&
-		 !strings.HasPrefix(url, "https://") {
-			
-			url = "https://" + url
-		}
+		!strings.HasPrefix(url, "https://") {
+
+		url = "https://" + url
+	}
 	return url
 }
 
 func CreateShortUrl(c *gin.Context) {
+
 	var creationRequest UrlCreationRequest
+
 	if err := c.ShouldBindJSON(&creationRequest); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error(),
@@ -31,9 +35,7 @@ func CreateShortUrl(c *gin.Context) {
 		return
 	}
 
-	longUrl := creationRequest.LongUrl
-
-	longUrl = normalizeUrl(longUrl)
+	longUrl := normalizeUrl(creationRequest.LongUrl)
 
 	shortUrl := shortener.GenerateShortLink(
 		longUrl,
@@ -46,12 +48,15 @@ func CreateShortUrl(c *gin.Context) {
 		creationRequest.UserId,
 	)
 
+	host := os.Getenv("BASE_URL")
 
-	host := "http://localhost:9808/"
-	
+	if host == "" {
+		host = "http://localhost:8080/"
+	}
+
 	c.JSON(200, gin.H{
-		"message": "short url created succesfully",
-		"short_url": host +shortUrl,
+		"message":   "short url created successfully",
+		"short_url": host + shortUrl,
 	})
 }
 
